4a0a3ebbedcb8d0be60490eee198a24299fdcd69
mbuella <marlon.b.buella@gmail.com>
2017-05-02 23:30:31 +0800

    Add codename for each socket

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+	//if there are more than two 
+	//members we can start pairing
   console.log('a user connected');
+  // console.log(socket.id);
 
-  //disconnect
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat intro', function(codename){
+	users[socket.id] = codename;
+    console.log('Codename: '+codename);
+    //inform all active chatters
+	socket.broadcast.emit('chat intro', codename);
+
+    //listen for disconnects
+	socket.on('disconnect', function(){
+		console.log(users[socket.id]+' has disconnected');
+	});
+
+	socket.on('chat message', function(msg){
+		console.log(users[socket.id]+': ' + msg);
+		socket.broadcast.emit('chat message', {
+			'cn': users[socket.id],
+			'text': msg
+		});
+	});
+
   });
 
   //broadcast chat msg  
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    socket.broadcast.emit('chat message', msg);
-  });
 
-  //private message
+  //private message to random 1
+
+  //private message to random 2
 
 });
 
